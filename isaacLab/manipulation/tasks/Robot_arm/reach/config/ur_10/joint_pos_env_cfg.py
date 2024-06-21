530a0ea325277c1b8d3d51b8c9b9ac3460a1db1d
NathanWu7 <nathan.wuqw@gmail.com>
2024-06-21 16:00:31 +0800

    Update joint_pos_env_cfg.py

--- isaacLab/manipulation/tasks/Robot_arm/reach/config/ur_10/joint_pos_env_cfg.py
+++ isaacLab/manipulation/tasks/Robot_arm/reach/config/ur_10/joint_pos_env_cfg.py
@@ -7,8 +7,10 @@ import math
 
 from omni.isaac.lab.utils import configclass
 
-import omni.isaac.lab_tasks.manager_based.manipulation.reach.mdp as mdp
-from omni.isaac.lab_tasks.manager_based.manipulation.reach.reach_env_cfg import ReachEnvCfg
+#import omni.isaac.lab_tasks.manager_based.manipulation.reach.mdp as mdp
+import isaacLab.manipulation.tasks.Robot_arm.reach.mdp as mdp
+from isaacLab.manipulation.tasks.Robot_arm.reach.reach_env_cfg import ReachEnvCfg
+#from omni.isaac.lab_tasks.manager_based.manipulation.reach.reach_env_cfg import ReachEnvCfg
 
 ##
 # Pre-defined configs
